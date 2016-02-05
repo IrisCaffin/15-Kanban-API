9b485bb025ab32139e5d7e94c55890623983f063
Iris Caffin <iriscaffin@gmail.com>
2016-02-04 21:55:37 -0800

    FrontEnd Day1 TBC

---
+++ 15-Kanban-API/Kanban.Web/js/app.js
@@ -0,0 +1,19 @@
+﻿// The Angular Module
+angular.module('kanban', ['ngResource']);
+
+angular.module('kanban').value('apiUrl', 'http://localhost:50026/api');
+
+angular.module('kanban').controller('IndexController', function ($scope, $resource, apiUrl) {
+
+    var ListResource = $resource(apiUrl + '/list/:listId', { listId: '@id'});
+
+    var lists = ListResource.query();
+
+    $scope.lists = lists;
+
+    //var User = $resource('/user/:userId', {userId:'@id'});
+    //var user = User.get({userId:123}, function() {
+    //    user.abc = true;
+    //    user.$save();
+    //});
+});
